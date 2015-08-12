00fd244c9cc958a59d76169e1bf7e0bc33783cc8
Rahul Verma <rv@testingperspective.com>
2015-08-12 18:57:51 +0530

    Cleaned up Custom DS based DD test

--- Unitee-Examples/src/in/unitee/ex/datadriven/CustomDataSourceBasedDDTestSuite.java
+++ Unitee-Examples/src/in/unitee/ex/datadriven/CustomDataSourceBasedDDTestSuite.java
@@ -1,26 +1,22 @@
 package in.unitee.ex.datadriven;
-
+ 
 import java.util.HashMap;
 import in.unitee.lib.test.DDTestSuite;
-
+ 
 public class CustomDataSourceBasedDDTestSuite extends DDTestSuite{
+ 
 	private int strToInt(Object input){
-		return Integer.parseInt((String) input);
+		return Integer.parseInt((String)input);
 	}
-
+	
 	public void init() throws Exception{
-		logger.info("called init()");
 		setDataSource(new CustomDataSource());
 	}
-
+		
 	public void repeat(HashMap<String,Object> map){
-		logger.info("repeating test");
-		System.out.println(map);
-		if (map!=null){
-			int left = strToInt(map.get("Left"));
-			int right = strToInt(map.get("Right"));
-			int expectedSum = strToInt(map.get("Sum"));
-			checker.assertEquals(expectedSum, left+right);
-		}
+		int left = strToInt(map.get("Left"));
+		int right = strToInt(map.get("Right"));
+		int expectedSum = strToInt(map.get("Sum"));
+		checker.assertEquals(expectedSum, left+right);
 	}
 }
